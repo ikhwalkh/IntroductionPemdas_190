216c78bed0a375338ff182099a49f7e4843835ad
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-02-23 16:49:41 +0700

    int age2; age2 = 40 cout << age2;

--- praktikum.cpp
+++ praktikum.cpp
@@ -6,11 +6,14 @@ string name;
 int main(){
     int age;
     int age1;
+    int age2;
     age = 30;
     age1 = 39;
+    age2 = 40;
 
     cout << "halo dunia" << endl;
     cout << 20 << endl;
     cout << age;
     cout << age1;
+    cout << age2;
 }
