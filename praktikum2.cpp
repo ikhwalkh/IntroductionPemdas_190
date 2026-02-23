d34f1bb27533a74554150c223e514019284c5ddc
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-02-23 16:45:34 +0700

    #include <iostream> using namespace std;
    int main(){
        int p, l, luas;
    
        cout << "masukkan panjang";
        cin >> p;
    
        cout << "masukkan lebar";
        cin >> l;
    
        luas = p*l;
    
        cout << "hasilnya" << luas;
    }

---
+++ praktikum2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p, l, luas;
+
+    cout << "masukkan panjang";
+    cin >> p;
+
+    cout << "masukkan lebar";
+    cin >> l;
+
+    luas = p*l;
+
+    cout << "hasilnya" << luas;
+}
