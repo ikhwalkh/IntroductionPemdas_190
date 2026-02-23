5106e72f7f59579d2cc4d5796aba1b6725f2b688
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-02-23 16:29:28 +0700

    first commit

---
+++ praktikum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+string name; 
+
+int main(){
+    int age;
+    int age1;
+    age = 20;
+    age1 = 30;
+
+    cout << "halo world" << endl;
+    cout << 99 << endl;
+    cout << age;
+    cout << age1;
+}
